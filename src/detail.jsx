f58c0a69b10cbc83ca03b9b3c187cb19a031a6cf
Nimrah Ashiq <nimoashiq687@gmail.com>
2024-05-06 11:47:47 +0500

    Add files via upload

---
+++ src/detail.jsx
@@ -0,0 +1,32 @@
+export const Data = [
+  {
+    id: 2,
+    firstName: "Nimrah",
+    lastName: "Ashiq",
+    age: 23,
+  },
+  {
+    id: 5,
+    firstName: "sumreen",
+    lastName: "waris",
+    age: 18,
+  },
+  {
+    id: 3,
+    firstName: "sidra",
+    lastName: "Ashiq",
+    age: 22,
+  },
+  {
+    id: 1,
+    firstName: "Farah",
+    lastName: "Imtiaz",
+    age: 25,
+  },
+  {
+    id: 4,
+    firstName: "Maryam",
+    lastName: "Ashiq",
+    age: 14,
+  },
+];
